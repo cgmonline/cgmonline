25af7a0716a67d19f6739c8db9b4b71f913a26e3
Jinliang Yang <yangjl0930@gmail.com>
2017-08-15 12:28:25 -0500

    logo files

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+cd docs
 # Add changes to git.
 git add .
 
