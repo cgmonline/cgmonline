ea6cc2b6951eb4cdc1f89ee5ee431e468d12e25e
Jinliang Yang <yangjl0930@gmail.com>
2019-03-21 13:58:54 -0500

    hugo re-ran

--- docs/js/settime.js
+++
@@ -1,5 +0,0 @@
-var nextYear = moment.tz("2018-01-01 00:00", "America/Sao_Paulo");
-
-$('#clock').countdown(nextYear.toDate(), function(event) {
-  $(this).html(event.strftime('%D days %H:%M:%S'));
-});
